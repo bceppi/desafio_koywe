8724414fa64bcf2736cfa285b03c1c9169aa029e
Bastian Ceppi <bceppi@ordename.cl>
2023-02-20 20:22:05 -0300

    feat(backend): code cleaning

--- backend/src/helpers.ts
+++ backend/src/helpers.ts
@@ -1,7 +1,100 @@
+import fetch from "node-fetch";
+
 export const isInvalidCrypto = (crypto: string) => {
   return crypto.toLowerCase() != "btc" && crypto.toLowerCase() != "eth";
 };
 
 export const isInvalidAmount = (amount: number) => {
   return isNaN(amount) || amount < 0;
+};
+
+export const getExchangeUrl = (exchange: string, crypto: string) => {
+  if (exchange == "binance") {
+    return `https://api4.binance.com/api/v3/depth?symbol=${crypto.toUpperCase()}USDT`;
+  } else {
+    return `https://api.exchange.coinbase.com/products/${crypto.toUpperCase()}-USDT/book?level=2`;
+  }
+};
+
+export const getPriceFromExchanges = async (crypto: string, amount: number) => {
+  const [binancePrice, coinbasePrice] = await Promise.all([
+    getPriceFromBinance(crypto, amount),
+    getPriceFromCoinbase(crypto, amount),
+  ]);
+  return [binancePrice, coinbasePrice];
+};
+
+const getPriceFromBinance = async (crypto: string, amount: number) => {
+  let url = getExchangeUrl("binance", crypto);
+  let totalPrice = 0;
+  await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      totalPrice = getPriceFromBook(data.asks, amount);
+    });
+  return totalPrice;
+};
+
+export const getPriceFromCoinbase = async (crypto: string, amount: number) => {
+  let url = getExchangeUrl("coinbase", crypto);
+  let totalPrice = 0;
+  await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      totalPrice = getPriceFromBook(data.asks, amount);
+    });
+  return totalPrice;
+};
+
+const getPriceFromBook = (asks: string[][], requiredQuantity: number) => {
+  let quantityCounter = 0;
+  let totalPrice = 0;
+  let askPrice: number;
+  let askQuantity: number;
+  let diffQuantity: number;
+  for (const ask of asks) {
+    askPrice = parseFloat(ask[0]);
+    askQuantity = parseFloat(ask[1]);
+    if (quantityCounter + askQuantity > requiredQuantity) {
+      diffQuantity = requiredQuantity - quantityCounter;
+      totalPrice += diffQuantity * askPrice;
+      quantityCounter += diffQuantity;
+      return totalPrice;
+    } else {
+      quantityCounter += askQuantity;
+      totalPrice += askQuantity * askPrice;
+    }
+  }
+  if (quantityCounter < requiredQuantity) {
+    throw new Error(
+      "No existen suficientes opciones de venta para el monto solicitado."
+    );
+  }
+  return totalPrice;
+};
+
+export const getBestExchangeFromPrice = (
+  binancePrice: number,
+  coinbasePrice: number
+) => {
+  let usdAmount: number;
+  let bestExchange: string;
+  if (binancePrice <= coinbasePrice) {
+    usdAmount = binancePrice;
+    bestExchange = "binance";
+  } else {
+    usdAmount = coinbasePrice;
+    bestExchange = "coinbase";
+  }
+  return { usdAmount: usdAmount, bestExchange: bestExchange };
 };
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,81 +1,12 @@
 import Router from "@koa/router";
-import { isInvalidAmount, isInvalidCrypto } from "../helpers";
-import fetch from "node-fetch";
+import {
+  isInvalidAmount,
+  isInvalidCrypto,
+  getPriceFromExchanges,
+  getBestExchangeFromPrice,
+} from "../helpers";
 
 export const router = new Router();
-
-const getPriceFromBook = (asks: string[][], requiredQuantity: number) => {
-  let quantityCounter = 0;
-  let totalPrice = 0;
-  let askPrice: number;
-  let askQuantity: number;
-  let diffQuantity: number;
-  for (const ask of asks) {
-    askPrice = parseFloat(ask[0]);
-    askQuantity = parseFloat(ask[1]);
-    if (quantityCounter + askQuantity > requiredQuantity) {
-      diffQuantity = requiredQuantity - quantityCounter;
-      totalPrice += diffQuantity * askPrice;
-      quantityCounter + diffQuantity;
-      return totalPrice;
-    } else {
-      quantityCounter += askQuantity;
-      totalPrice += askQuantity * askPrice;
-    }
-  }
-  return totalPrice;
-};
-
-const getExchangeUrl = (exchange: string, crypto: string) => {
-  if (exchange == "binance") {
-    return `https://api4.binance.com/api/v3/depth?symbol=${crypto.toUpperCase()}USDT`;
-  } else {
-    return `https://api.exchange.coinbase.com/products/${crypto.toUpperCase()}-USDT/book?level=2`;
-  }
-};
-
-const getPriceFromBinance = async (crypto: string, amount: number) => {
-  let url = getExchangeUrl("binance", crypto);
-  let totalPrice = 0;
-  await fetch(url, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(` binance${data.asks}`);
-      console.log(` binance: ${data.length}`);
-      totalPrice = getPriceFromBook(data.asks, amount);
-    });
-  return totalPrice;
-};
-
-const getPriceFromCoinbase = async (crypto: string, amount: number) => {
-  let url = getExchangeUrl("coinbase", crypto);
-  let totalPrice = 0;
-  await fetch(url, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(` coinbase: ${data.asks}`);
-      totalPrice = getPriceFromBook(data.asks, amount);
-    });
-  return totalPrice;
-};
-
-const getPriceFromExchanges = async (crypto: string, amount: number) => {
-  const [binancePrice, coinbasePrice] = await Promise.all([
-    getPriceFromBinance(crypto, amount),
-    getPriceFromCoinbase(crypto, amount),
-  ]);
-  return [binancePrice, coinbasePrice];
-};
 
 router.get("/best-available-exchange", async (ctx, next) => {
   const amount = ctx.request.query.amount as string;
@@ -86,31 +17,55 @@ router.get("/best-available-exchange", async (ctx, next) => {
     };
     ctx.status = 400;
   } else {
-    if (isInvalidCrypto(crypto) || isInvalidAmount(parseInt(amount))) {
+    if (isInvalidCrypto(crypto)) {
       ctx.body = {
         message: "Crypto moneda inválida. Solo se acepta BTC o ETH.",
       };
       ctx.status = 400;
-    } else {
-      const [binancePrice, coinbasePrice] = await getPriceFromExchanges(
-        crypto,
-        parseInt(amount)
-      );
-      let usdAmount: number;
-      let bestExchange: string;
-      if (binancePrice <= coinbasePrice) {
-        usdAmount = binancePrice;
-        bestExchange = "binance";
-      } else {
-        usdAmount = coinbasePrice;
-        bestExchange = "coinbase";
-      }
+    } else if (isInvalidAmount(parseInt(amount))) {
       ctx.body = {
-        crypto: crypto.toUpperCase(),
-        amount: amount,
-        usdAmount: usdAmount,
-        exchange: bestExchange,
+        message: "Monto inválido.",
       };
+      ctx.status = 400;
+    } else {
+      try {
+        const [binancePrice, coinbasePrice] = await getPriceFromExchanges(
+          crypto,
+          parseInt(amount)
+        );
+        let { usdAmount, bestExchange } = getBestExchangeFromPrice(
+          binancePrice,
+          coinbasePrice
+        );
+        ctx.body = {
+          crypto: crypto.toUpperCase(),
+          amount: amount,
+          usdAmount: usdAmount.toFixed(2),
+          exchange: bestExchange,
+        };
+      } catch (e) {
+        let result = "";
+        if (typeof e === "string") {
+          result = e.toUpperCase();
+        } else if (e instanceof Error) {
+          result = e.message;
+        }
+        ctx.body = { message: result };
+      }
+      ctx.status = 200;
     }
   }
 });
+
+// const isThereEnoughAsks = (asks: string[][], requiredQuantity: number) => {
+//   let quantityCounter = 0;
+//   let askQuantity: number;
+//   for (const ask of asks) {
+//     askQuantity = parseFloat(ask[1]);
+//     quantityCounter += askQuantity;
+//     if (quantityCounter >= requiredQuantity) {
+//       return true;
+//     }
+//   }
+//   return false;
+// };
